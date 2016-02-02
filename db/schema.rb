098d6cd882e9f6d06a0258ca59cf80c584cf2d04
craig95 <u15029779@tuks.co.za>
2016-02-02 23:26:41 +0200

    Added blog scaffold. And added bootstrap for styling.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202053310) do
+ActiveRecord::Schema.define(version: 20160202191844) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "content"
   end
 
 end
